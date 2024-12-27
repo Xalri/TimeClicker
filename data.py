1bc372f3c4264347c9daf1e30a7926e874c43d0c
unknown <samine78gaming@gmail.com>
2024-12-27 21:44:20 +0100

    v1

---
+++ data.py
@@ -0,0 +1,21 @@
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+# Basics
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+# Colorful
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+PURPLE = (255, 0, 255)
+# Special
+ORANGE = (255, 127, 0)
+MAGENTA = (255, 0, 255)
+PINK = (255, 192, 203)
+BROWN = (88, 41, 0)
+GRAY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+SILVER = (192, 192, 192)
+GOLD = (218, 165, 32)
+
+BACKGROUND_COLOR = BROWN
--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-from graphics_nsi import *
-from pprint import pprint
-import pygame as pg
-import ctypes
-import os
-
-#define constant
-SCREEN = {"width": ctypes.windll.user32.GetSystemMetrics(0), "height": ctypes.windll.user32.GetSystemMetrics(1)}
-NAVIGUATION_BAR_HEIGHT = 29
-
-# place window in position 0,0
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, NAVIGUATION_BAR_HEIGHT)
+import pygame
+from pygame._sdl2 import Window
+from data import *
 
 
+screen = pygame.display.set_mode((720, 480), pygame.RESIZABLE)
 
+Window.from_display_module().maximize()
 
+pygame.display.set_caption('Time Clicker')
 
+screen.fill(BACKGROUND_COLOR)
 
-def main():
-    init_graphic(SCREEN["width"], SCREEN["height"] - NAVIGUATION_BAR_HEIGHT, "Binary Clicker")
+pygame.display.flip()
+
+running = True
+
+while running:
     
+    for event in pygame.event.get():
     
-    wait_escape("Appuyer sur Echap pour terminer")
-if __name__ == "__main__":
-    main()
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            running = False
