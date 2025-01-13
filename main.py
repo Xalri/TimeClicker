9e41679e6e4ce24afd6151c7693ce48ee7e07e68
unknown <samine78gaming@gmail.com>
2025-01-13 14:40:11 +0100

    ajout option de click, sauvegarde auto

--- main.py
+++ main.py
@@ -4,8 +4,45 @@ from data import *
 from Buttons import Button
 import time
 from Logger import Logger
-import buildings
-from utils import adapt_size_height, adapt_size_width, load_image, get_number_font, get_text_font, get_timeline_font
+# import buildings
+from utils import adapt_size_height, adapt_size_width, load_image, get_number_font, get_text_font, get_timeline_font, resource_path
+import os
+from base64 import b64decode, b64encode
+
+# Get the path to the AppData/Local directory
+appdata_path = os.path.join(os.getenv('LOCALAPPDATA'), 'TimeClicker')
+src_dir = resource_path("src")
+
+# Check if the directory exists, if not, create it
+if not os.path.exists(appdata_path):
+    os.makedirs(appdata_path)
+    
+    
+def save_data(timeUnits, tps, timeline, clicker_amount, buildings):            
+    with open(os.path.join(appdata_path, 'data'), 'w') as f:
+        data = f"{timeUnits}\n{tps}\n{timeline}\n{clicker_amount}\n{buildings}"
+        data = b64encode(data.encode())
+        data = data.decode().replace("=", "").replace("=", "")
+        data = data[::-1]
+        f.write(data)
+
+
+def get_data():
+    if not os.path.exists(os.path.join(appdata_path, 'data')):
+        save_data(0, 0, 0, 1, "buildings")
+    with open(os.path.join(appdata_path, 'data'), 'r') as f:
+        data = f.read()
+        data = data[::-1]
+        data = data + "=="
+        data = b64decode(data.encode()).decode()
+        data = data.split('\n')
+        timeUnits = float(data[0])
+        tps = float(data[1])
+        timeline = float(data[2])
+        clicker_amount = int(data[3])
+        buildings = data[4]
+        return timeUnits, tps, timeline, clicker_amount, buildings
+        
 
 # initializing
 LOGGER: Logger = Logger()
@@ -29,6 +66,7 @@ pg.display.flip()
 
 
 
+
 def crop_value(value: float):
     if value == 0.0:
         return 0
@@ -44,25 +82,29 @@ def crop_value(value: float):
 #         timeUnits = "".join([" " for _ in range(9 - len(timeUnits.replace(".", "")))]) + timeUnits
 #     return timeUnits
 
-def format_timeUnits(timeUnits: float):
+def format_timeUnits(timeUnits: float, n):
     timeUnits = crop_value(timeUnits)
     if timeUnits < 1000:
-        return "".join([" " for _ in range(11 - len(str(timeUnits)))]) + str(timeUnits)
+        return "".join([" " for _ in range(n+2 - len(str(timeUnits)))]) + str(timeUnits)
     for unit, factor in zip(['k', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'Ud',  'Dd', 'Td'], [10**i for i in range(3, (14 * 3)+1, 3)]):
         if timeUnits < factor * 1000:
             timeUnits = f"{timeUnits / factor:.1f}{unit}".rstrip('.0')
-            return "".join([" " for _ in range(9 - len(timeUnits.replace(".", "").replace(unit, "")))]) + timeUnits
+            return "".join([" " for _ in range(n - len(timeUnits.replace(".", "").replace(unit, "")))]) + timeUnits
     return f"{timeUnits:.0f}"
+
+def format_time_no_convertion(value: int, n: int):
+    return "".join([" " for _ in range(n+2 - len(str(value)))]) + str(value)    
 
 
 
 def main():
     # define game values
-    timeUnits: float = 1
-    tps: float = 0
+    timeUnits, tps, timeline, clicker_amount, buildings = get_data()
+    # timeUnits: float = 1
+    # tps: float = 0
     
-    clicker_amount: float = 1
-    timeline: int = 0
+    # clicker_amount: float = 1
+    # timeline: int = 0
     
     current_frame: int = 0
     framerate: int =10
@@ -75,6 +117,7 @@ def main():
     def increment_timeUnits(amount):
         nonlocal timeUnits, clicker_amount
         timeUnits += amount    
+
         
 
 
@@ -86,17 +129,22 @@ def main():
         
         
         
-        TIMELINE_FONT: pygame.font = pygame.font.Font('src/fonts/LetterGothicStd-Bold.ttf', int(adapt_size_height(124, h)))
-        NUMBER_FONT: pygame.font = pygame.font.Font('src/fonts/LetterGothicStd-Bold.ttf', int(adapt_size_height(65, h)))
-        TEXT_FONT: pygame.font = pygame.font.Font('src/fonts/FranklinGothicHeavyRegular.ttf', 33)
+        # TIMELINE_FONT: pygame.font = pygame.font.Font('src/fonts/LetterGothicStd-Bold.ttf', int(adapt_size_height(124, h)))
+        # NUMBER_FONT: pygame.font = pygame.font.Font('src/fonts/LetterGothicStd-Bold.ttf', int(adapt_size_height(65, h)))
+        # TEXT_FONT: pygame.font = pygame.font.Font('src/fonts/FranklinGothicHeavyRegular.ttf', 33)
         
         
         
         
-        timeUnits_text: pg.Surface = get_number_font(65, h).render(f"{format_timeUnits(timeUnits)}", True, RED)
-        tps_text: pg.Surface = get_number_font(50, h).render(f"{format_timeUnits(tps)}", True, RED)
+        timeUnits_text: pg.Surface = get_number_font(65, h).render(f"{format_timeUnits(timeUnits, 9)}", True, RED)
+        tps_text: pg.Surface = get_number_font(50, h).render(f"{format_timeUnits(tps, 9)}", True, RED)
         
-        clicker_button: Button = Button((adapt_size_width(715, w), adapt_size_height(304, h), adapt_size_width(403, w), adapt_size_height(400, h)), BLUE, lambda: increment_timeUnits(clicker_amount), 250, True)
+        timeline_text: pg.Surface = get_timeline_font(124, h).render(f"{format_time_no_convertion(timeline,3)}", True, YELLOW_GREEN)
+        # timeline_text: pg.Surface = get_timeline_font(124, h).render(f"{timeline}", True, YELLOW_GREEN)
+        
+        clicker_button: Button = Button(
+            (adapt_size_width(715, w), adapt_size_height(304, h), adapt_size_width(403, w), adapt_size_height(400, h))
+            , f"{src_dir}/img/hourglass.png", lambda: increment_timeUnits(clicker_amount), 250, False, 0.65, 10, True)
         
 
 
@@ -115,6 +163,7 @@ def main():
         for event in pg.event.get():
         
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                save_data(timeUnits, tps, timeline, clicker_amount, buildings)
                 running = False
             
             clicker_button.get_event(event)
@@ -126,25 +175,25 @@ def main():
         screen.fill(BACKGROUND_COLOR)
         
         
-        timeline_image: pg.surface = load_image('src/img/timeline.png', w, h)
+        timeline_image: pg.surface = load_image(f"{src_dir}/img/timeline.png", w, h)
         screen.blit(timeline_image, (adapt_size_width(45, w), adapt_size_height(45, h)))
         
-        upgrade_image: pg.surface = load_image('src/img/upgrade.png', w, h)
+        upgrade_image: pg.surface = load_image(f"{src_dir}/img/upgrade.png", w, h)
         screen.blit(upgrade_image, (adapt_size_width(45, w), adapt_size_height(245, h)))
         
-        temporal_matrix_image: pg.surface = load_image('src/img/temporal matrix.png', w, h)
+        temporal_matrix_image: pg.surface = load_image(f"{src_dir}/img/temporal matrix.png", w, h)
         screen.blit(temporal_matrix_image, (adapt_size_width(605, w), adapt_size_height(45, h)))
         
-        human_skill_and_boost: pg.surface = load_image('src/img/human_skill+boost.png', w, h)
+        human_skill_and_boost: pg.surface = load_image(f"{src_dir}/img/human_skill+boost.png", w, h)
         screen.blit(human_skill_and_boost, (adapt_size_width(1265, w), adapt_size_height(45, h)))
         
-        shop_image: pg.surface = load_image('src/img/shop.png', w, h)
+        shop_image: pg.surface = load_image(f"{src_dir}/img/shop.png", w, h)
         screen.blit(shop_image, (adapt_size_width(1475, w), adapt_size_height(45, h)))
         
-        red_cable_image: pg.surface = load_image('src/img/red_cable_on.png', w, h)
+        red_cable_image: pg.surface = load_image(f"{src_dir}/img/red_cable_on.png", w, h)
         screen.blit(red_cable_image, (adapt_size_width(1285, w), adapt_size_height(860, h)))
         
-        blue_cable_image: pg.surface = load_image('src/img/blue_cable_on.png', w, h)
+        blue_cable_image: pg.surface = load_image(f"{src_dir}/img/blue_cable_on.png", w, h)
         screen.blit(blue_cable_image, (adapt_size_width(1285, w), adapt_size_height(935, h)))
                 
         
@@ -165,6 +214,8 @@ def main():
         # draw text
         screen.blit(timeUnits_text, (adapt_size_width(700, w), adapt_size_height(840, h)))
         screen.blit(tps_text, (adapt_size_width(700, w), adapt_size_height(177, h)))
+        
+        screen.blit(timeline_text, (adapt_size_width(90, w), adapt_size_height(87, h)))
         
         clicker_button.render(screen)
 
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+import os
 
 def adapt_size_height(size, height):
     return size / 1080 * height
@@ -9,10 +11,19 @@ def load_image(path, width, height):
     image: pygame.surface = pygame.image.load(path).convert_alpha()
     return pygame.transform.scale(image, (adapt_size_width(image.get_width(), width), adapt_size_height(image.get_height(), height)))
 
+def resource_path(relative_path):
+    """ Get the absolute path to the resource, works for dev and for PyInstaller """
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS  # Temporary folder for PyInstaller
+    else:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path).replace("\\", "/")
 
 def get_timeline_font(size, height):
-    return pygame.font.Font('src/fonts/LetterGothicStd-Bold.ttf', int(adapt_size_height(size, height)))
+    return pygame.font.Font(f"{resource_path("src")}/fonts/LetterGothicStd-Bold.ttf", int(adapt_size_height(size, height)))
 def get_number_font(size, height):
-    return pygame.font.Font('src/fonts/LetterGothicStd-Bold.ttf', int(adapt_size_height(size, height)))
-def  get_text_font(size, height):
-    return pygame.font.Font('src/fonts/FranklinGothicHeavyRegular.ttf', int(adapt_size_height(size, height)))
+    return pygame.font.Font(f"{resource_path("src")}/fonts/LetterGothicStd-Bold.ttf", int(adapt_size_height(size, height)))
+def get_text_font(size, height):
+    return pygame.font.Font(f"{resource_path("src")}/fonts/FranklinGothicHeavyRegular.ttf", int(adapt_size_height(size, height)))
+
